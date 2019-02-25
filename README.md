47db249d37ae0cb6db413565699235050d5aed6c
Matt Worzala <WorzalaM02@gmail.com>
2019-02-24 23:03:51 -0500

    config has been removed completely. kicks now balance players correctly, but crashes have a slightly odd behavior from velocity that needs more investigating.

--- README.md
+++ README.md
@@ -4,10 +4,12 @@ A simple plugin to balance player counts on a Velocity proxy.
 ## Installation
   * Download the latest release from the [releases tab](https://github.com/bhopahk/VelocityLoadBalancer/releases).
   * Put the jar in your Velocity plugins folder
-  * Start the server once
-  * Add the desired lobby servers to the `config.toml` file located in `plugins/loadbalancer/`.
   * Restart Velocity
+  
+## Configuration
+LoadBalancer does not have a configuration file and uses the fallback servers defined inside the Velocity server to decide which server the player will be connected to.  
 
 ## Building LoadBalancer
-  * Pull the repository in whichever way you are most comfortable
+The releases tab will be updated for notable changes to the plugin, however if you would like the have the bleeding edge version you can build the plugin yourself.
+  * Pull the repository using whichever method you are most comfortable
   * Run the `build` gradle task using a local gradle distribution or the included gradle wrapper.
--- src/main/java/me/bhop/velocityloadbalancer/LoadBalancerPlugin.java
+++ src/main/java/me/bhop/velocityloadbalancer/LoadBalancerPlugin.java
@@ -1,9 +1,9 @@
 package me.bhop.velocityloadbalancer;
 
 import com.google.inject.Inject;
-import com.moandjiezana.toml.Toml;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.connection.DisconnectEvent;
+import com.velocitypowered.api.event.player.KickedFromServerEvent;
 import com.velocitypowered.api.event.player.ServerPreConnectEvent;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.plugin.Plugin;
@@ -12,61 +12,35 @@ import com.velocitypowered.api.proxy.ProxyServer;
 import com.velocitypowered.api.proxy.server.RegisteredServer;
 import org.slf4j.Logger;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Plugin(
         id = "loadbalancer",
         name = "LoadBalancer",
-        version = "1.0",
+        version = "1.1",
         authors = {"bhop_"},
         description = "A server load balancer for Velocity."
 )
 public class LoadBalancerPlugin {
     private final ProxyServer proxyServer;
-    private final Path dataDirectory;
     private final Logger logger;
 
-    private final List<String> lobbies = new ArrayList<>();
+    private final List<RegisteredServer> lobbies = new ArrayList<>();
     private final List<UUID> connectedPlayers = new ArrayList<>();
 
     @Inject
     public LoadBalancerPlugin(ProxyServer proxyServer, Logger logger, @DataDirectory Path dataDirectory) {
         this.proxyServer = proxyServer;
         this.logger = logger;
-        this.dataDirectory = dataDirectory;
     }
 
     @Subscribe
     public void onProxyInit(ProxyInitializeEvent event) {
-        Path confFile = dataDirectory.resolve("config.toml");
-        try {
-            if (!Files.exists(dataDirectory))
-                Files.createDirectory(dataDirectory);
-            if (!Files.exists(confFile)) {
-                try (InputStream stream = this.getClass().getClassLoader().getResourceAsStream("config.toml")) {
-                    if (stream != null)
-                        Files.copy(stream, confFile);
-                    else
-                        throw new RuntimeException();
-                }
-            }
-        } catch (IOException e) {
-            throw new RuntimeException("Failed to create config file.", e);
-        }
-        Toml toml;
-        try (BufferedReader reader = Files.newBufferedReader(confFile)) {
-            toml = new Toml().read(reader);
-        } catch (IOException e) {
-            throw new RuntimeException("Failed to read config file.", e);
-        }
-
-        lobbies.addAll(toml.getList("lobby_servers"));
-        logger.info("Loaded LoadBalancer. The following servers are treated as lobbies: " + lobbies.toString());
+        for (String serverName : proxyServer.getConfiguration().getAttemptConnectionOrder())
+            proxyServer.getServer(serverName).ifPresent(lobbies::add);
+        logger.info("Loaded LoadBalancer. The following servers are treated as lobbies: " + lobbies.stream().map(server -> server.getServerInfo().getName()).collect(Collectors.joining(", ")));
     }
 
     @Subscribe
@@ -75,10 +49,18 @@ public class LoadBalancerPlugin {
     }
 
     @Subscribe
+    public void onKick(KickedFromServerEvent event) {
+        if (event.kickedDuringServerConnect())
+            return;
+        Optional<RegisteredServer> opt = proxyServer.getAllServers().stream().filter(server -> server != event.getServer()).filter(lobbies::contains).min(Comparator.comparingInt(s -> s.getPlayersConnected().size()));
+        opt.ifPresent(registeredServer -> event.setResult(KickedFromServerEvent.RedirectPlayer.create(registeredServer)));
+    }
+
+    @Subscribe
     public void onJoin(ServerPreConnectEvent event) {
         if (!connectedPlayers.contains(event.getPlayer().getUniqueId())) {
             connectedPlayers.add(event.getPlayer().getUniqueId());
-            Optional<RegisteredServer> opt = proxyServer.getAllServers().stream().filter(server -> lobbies.contains(server.getServerInfo().getName())).sorted(Comparator.comparingInt(s -> s.getPlayersConnected().size())).findFirst();
+            Optional<RegisteredServer> opt = proxyServer.getAllServers().stream().filter(lobbies::contains).min(Comparator.comparingInt(s -> s.getPlayersConnected().size()));
             if (!opt.isPresent()) {
                 logger.warn("No valid lobby servers were detected, so joining player '" + event.getPlayer().getUsername() + "' was connected to the default server.");
                 return;
